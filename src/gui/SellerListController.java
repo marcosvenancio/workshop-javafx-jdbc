fef36843567939bf0cc6c6dc8c1779c184685356
Marcos Venancio <viniciusdacosta@hotmail.com.br>
2020-10-09 15:45:34 -0300

    SellerForm

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -103,28 +108,26 @@ public class SellerListController implements Initializable, DataChangeListener {
 	}
 
 	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-//			Pane pane = loader.load();
-//
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj); // associando ao objeto department - associando as caixas de texto
-//			controller.setSellerService(new SellerService()); // inhjeção de dependencia
-//			controller.subscribeDataChangeListener(this);
-//			controller.updateFormData(); // atualizando
-//
-//			Stage dialogStage = new Stage(); // novo palco e abaixo as configurações
-//			dialogStage.setTitle("Enter department data"); // título da janela
-//			dialogStage.setScene(new Scene(pane)); // novo stage, vai precisar de uma nova cena, e o elemento raiz é o
-//													// pane
-//			dialogStage.setResizable(false); // a janela nesse caso na pode ser redimensionada
-//			dialogStage.initOwner(parentStage); // parent stage = stage pai desta janela
-//			dialogStage.initModality(Modality.WINDOW_MODAL); // esse método indica o comportamento modal da janela - ela
-//																// fica travada
-//			dialogStage.showAndWait();
-//		} catch (IOException e) {
-//			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj); // associando ao objeto department - associando as caixas de texto
+			controller.setSellerService(new SellerService()); // inhjeção de dependencia
+			controller.subscribeDataChangeListener(this);
+			controller.updateFormData(); // atualizando
+
+			Stage dialogStage = new Stage(); // novo palco e abaixo as configurações
+			dialogStage.setTitle("Enter department data"); // título da janela
+			dialogStage.setScene(new Scene(pane)); // novo stage, vai precisar de uma nova cena, e o elemento raiz é o pane
+			dialogStage.setResizable(false); // a janela nesse caso na pode ser redimensionada
+			dialogStage.initOwner(parentStage); // parent stage = stage pai desta janela
+			dialogStage.initModality(Modality.WINDOW_MODAL); // esse método indica o comportamento modal da janela - ela fica travada
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
